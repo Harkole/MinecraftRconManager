7abf1d3a75e33748fd7e2200760d9dff0f784e49
Will Blackburn <harkole@gmail.com>
2022-01-21 23:44:44 +0000

    Create CommandRequest.cs

---
+++ src/Mc.Rcon.Api/Models/CommandRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mc.Rcon.Api.Models
+{
+    public class CommandRequest
+    {
+        /// <summary>
+        /// The password to authenticate against RCON with
+        /// </summary>
+        [Required]
+        public string? Password { get; set; }
+
+        /// <summary>
+        /// The server instruction to execute
+        /// </summary>
+        [Required]
+        public string? Instruction { get; set; }
+    }
+}
