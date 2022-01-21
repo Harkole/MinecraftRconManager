92e35b709c99d6116f0701a59fd419077ae39156
Will Blackburn <harkole@gmail.com>
2022-01-21 23:45:00 +0000

    Update CommandController.cs

--- src/Mc.Rcon.Api/Controllers/CommandController.cs
+++ src/Mc.Rcon.Api/Controllers/CommandController.cs
@@ -1,3 +1,5 @@
+using Mc.Rcon.Api.Interfaces;
+using Mc.Rcon.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mc.Rcon.Api.Controllers;
@@ -7,16 +9,29 @@ namespace Mc.Rcon.Api.Controllers;
 public class CommandController : ControllerBase
 {
     private readonly ILogger<CommandController> _logger;
+    private readonly IRconService _rconService;
 
-    public CommandController(ILogger<CommandController> logger)
+    public CommandController(ILogger<CommandController> logger, IRconService rconService)
     {
         _logger = logger;
+        _rconService = rconService;
     }
 
-    [HttpGet]
-    public string Get()
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult Post(CommandRequest commandRequest)
     {
         _logger.LogInformation($"Get called on {nameof(CommandController)}");
-        return "Hello World!";
+        
+        if (null == commandRequest.Password || null == commandRequest.Instruction)
+        {
+            return BadRequest();
+        }
+
+        _rconService.Authenticate(commandRequest.Password);
+        _rconService.SendCommand(commandRequest.Instruction, out string response);
+        _rconService.CloseConnection();
+
+        return Ok(response);
     }
 }
