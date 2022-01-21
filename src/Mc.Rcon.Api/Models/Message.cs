66255c14afd49e2048efc6507fdf546e5bf21284
Will Blackburn <harkole@gmail.com>
2022-01-21 22:53:51 +0000

    Model update and comments

--- src/Mc.Rcon.Api/Models/Message.cs
+++ src/Mc.Rcon.Api/Models/Message.cs
@@ -2,12 +2,24 @@
 {
     public class Message
     {
+        /// <summary>
+        /// Message length
+        /// </summary>
         public int Length { get; set; }
 
+        /// <summary>
+        /// Identity of the message
+        /// </summary>
         public int Id { get; set; }
 
-        public MessageTypes MessageType { get; set; }
+        /// <summary>
+        /// The type of message this is
+        /// </summary>
+        public MessageType MessageType { get; set; }
 
+        /// <summary>
+        /// The message payload/response body
+        /// </summary>
         public string Body { get; set; } = string.Empty;
     }
 }
