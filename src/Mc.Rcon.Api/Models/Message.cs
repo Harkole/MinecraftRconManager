55d41537c5ed7eda60fda5e14d7489febca79a18
Will Blackburn <harkole@gmail.com>
2022-01-21 22:32:46 +0000

    Added basic message model

---
+++ src/Mc.Rcon.Api/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace Mc.Rcon.Api.Models
+{
+    public class Message
+    {
+        public int Length { get; set; }
+
+        public int Id { get; set; }
+
+        public MessageTypes MessageType { get; set; }
+
+        public string Body { get; set; } = string.Empty;
+    }
+}
